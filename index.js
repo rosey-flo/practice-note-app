ace6cd6a5e73e860ad62a5a918b2013362cb785c
rosey-flo <emilyflo2012@yahoo.com>
2024-07-17 12:50:12 -0400

    basic class conversion

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-const user = {
-  name: 'emily',
-  age: 27
-};
-
-
-//constructor function - generates objects
-function User(name, age, hobbies) {
+class User {
+  constructor(name, age, hobbies) {
   this.name = name;
   this.age = age;
   this.hobbies = hobbies;
-
-  // must call the nested functions in the direct parent to make them work
+  }
   
+  getAge() {
+    return this.age;
+  }
+  haveBirthday() {
+    this.age++
+  }
 }
-User.prototype.getAge = function () {
-  return this.age;
-}
-User.prototype.haveBirthday = function () {
-  this.age++;
-}
-
 
 //these are the actual objects but can now be called
 const emily = new User('Emily', 27, ['bingo', 'fishing']);
 const bob = new User('Bob', 57, ['pickleball', 'phone']);
+
+console.log(emily)
 
 
 
